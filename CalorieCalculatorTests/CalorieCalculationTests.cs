d831ae308bb2e610929804c70ae9a6f36e735c48
echolimitless <echolimitless@outlook.com>
2025-01-25 04:11:32 +0900

    テストプロジェクト追加

---
+++ CalorieCalculatorTests/CalorieCalculationTests.cs
@@ -0,0 +1,43 @@
+using CalorieCalculator;
+
+namespace CalorieCalculatorTests
+{
+    /// <summary>
+    /// CalorieCalculationのテスト
+    /// </summary>
+    public class CalorieCalculationTests
+    {
+        /// <summary>
+        /// カロリー計算が正しいく行えているか
+        /// </summary>
+        [Theory]
+        [MemberData(nameof(CorrectCaloriesData))]
+        public void CorrectCalories(double protein, double fat, double carbohydrate, double expected)
+        {
+            // Arrange
+            var amountOfIntake = new AmountOfIntake
+            {
+                Protein = protein,
+                Fat = fat,
+                Carbohydrate = carbohydrate
+            };
+            var calorieCalculation = new CalorieCalculation();
+
+            // Act
+            var result = calorieCalculation.Execute(amountOfIntake);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// CorrectCaloriesのテストデータ
+        /// </summary>
+        public static TheoryData<double, double, double, double> CorrectCaloriesData =>
+            new TheoryData<double, double, double, double>
+            {
+                /// 通常ケース
+                { 2.5, 0.3, 37.1, 161 },
+            };
+    }
+}
