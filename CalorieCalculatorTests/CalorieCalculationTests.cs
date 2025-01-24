ada337d118549ab7a1bb0cc97e3654ab6cd187c4
echolimitless <echolimitless@outlook.com>
2025-01-25 04:51:29 +0900

    テストケース追加

--- CalorieCalculatorTests/CalorieCalculationTests.cs
+++ CalorieCalculatorTests/CalorieCalculationTests.cs
@@ -36,8 +36,18 @@ namespace CalorieCalculatorTests
         public static TheoryData<double, double, double, long> CorrectCaloriesData =>
             new TheoryData<double, double, double, long>
             {
-                /// 通常ケース
+                // 計算が正しく行えているか
                 { 2.5, 0.3, 37.1, 161 },
+                { 16.5, 10.0, 12.1, 204 },
+                // Proteinの四捨五入テスト
+                { 0.64, 0.0, 0.0, 2 },
+                { 0.65, 0.0, 0.0, 3 },
+                // Fatの四捨五入テスト+結果の四捨五入テスト
+                { 0.0, 0.54, 0.0, 5 },
+                { 0.0, 0.55, 0.0, 5 },
+                // Carbonydrateの四捨五入テスト
+                { 0.0, 0.0, 0.64, 2 },
+                { 0.0, 0.0, 0.65, 3 },
             };
     }
 }
