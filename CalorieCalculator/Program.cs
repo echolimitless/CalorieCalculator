ebd6af3402202a68b2e726f2cbee2fc830f1ae59
echolimitless <echolimitless@outlook.com>
2025-01-25 04:00:09 +0900

    カロリー計算完成

--- CalorieCalculator/Program.cs
+++ CalorieCalculator/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace CalorieCalculator
 {
-    internal class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var rice = new AmountOfIntake
+            {
+                Protein = 2.5,
+                Fat = 0.3,
+                Carbohydrate = 37.1
+            };
+
+            var natto = new AmountOfIntake
+            {
+                Protein = 16.5,
+                Fat = 10.0,
+                Carbohydrate = 12.1
+            };
+
+            var calorieCalculation = new CalorieCalculation();
+            var calories = calorieCalculation.Execute(rice);
+            calories += calorieCalculation.Execute(natto);
+            Console.WriteLine(calories);
         }
     }
 }
